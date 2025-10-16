ecfb6d227559d15abe48b0527935484c1911a770
thantzin <dev.thantzin@gmail.com>
2025-10-16 16:01:51 +0700

    Add Comment

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,15 @@ import 'flavors.dart';
 
 Future<void> main() async {
   /// RUN with $ flutter run --flavor production -t lib/main_prod.dart
+  /// <OR>
+  /// Open Run Configurations
+  /// Go to the top toolbar in Android Studio.
+  /// Click the dropdown next to the Run ▶️ button.
+  /// Click “Edit Configurations…”
+  /// Dart entrypoint  => lib/main_prod.dart
+  /// The --flavor production goes into Additional run args.
+  /// Click Apply, then OK.
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await FlavorManager().init(F.appFlavor);
